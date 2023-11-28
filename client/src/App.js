3a10e033dd96b5fab99c896f9a9d97d92a4a7d28
Jonathan Wehner <jonjwehner@gmail.com>
2023-11-28 03:13:10 -0500

    initial commit

---
+++ client/src/App.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import OtherPage from './OtherPage';
+import Fib from './Fib';
+
+function App() {
+  return (
+    <Router>
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />          
+        </header>
+        <div>
+          <Fib />
+          <Route path="/otherpage" component={OtherPage} />
+        </div>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
