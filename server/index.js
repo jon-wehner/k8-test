7e4d34f50ed1f0349e8c3252d8a8fdd11e634b47
Jonathan Wehner <jonjwehner@gmail.com>
2023-12-05 21:26:26 -0500

    update code

--- server/index.js
+++ server/index.js
@@ -54,11 +54,12 @@ app.get("/values/current", async (req, res) => {
 
 app.post("/values", async (req, res) => {
   const index = req.body.index;
+
   if (parseInt(index) > 40) {
     return res.status(422).send("Index too high");
   }
 
-  const hset = redisClient.hset("values", index, "Nothing yet!");
+  redisClient.hset("values", index, "Nothing yet!");
   redisPublisher.publish("insert", index);
   pgClient.query("INSERT INTO values(number) VALUES($1)", [index]);
 
